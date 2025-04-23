14d48ce24067fc4759594b6e2180b48a82bf0f24
tarunsankhla <tarun.sankhla.prc@gmail.com>
2025-04-23 13:54:33 -0400

    readme update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4">
+    <footer className="bg-gray-800 text-white py-4" style={{ padding: '20px 0' }}>
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
